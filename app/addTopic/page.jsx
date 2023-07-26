9e0dcf0a2fd5abce269e8b0c49f6a4255ecfd4e1
muhammad abdullah <abdullahbhatti3399@gmail.com>
2023-07-26 17:14:48 +0500

    second commit

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -2,7 +2,7 @@
 import {useState} from 'react';
 import {useRouter} from 'next/navigation';
 
-export default function addTopic(){
+export default function AddTopic(){
     const [title, setTitle]=useState('');
     const [description, setDescription]=useState('');
     const router=useRouter();
